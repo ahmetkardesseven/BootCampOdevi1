6086ae0ed671b4cf720ea6405fbd10bc9f9da163
ahmetkardesseven <103737133+ahmetkardesseven@users.noreply.github.com>
2023-01-05 17:42:18 +0300

    Add files via upload

---
+++ ahmetkardesseven1.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+
+var sehir:String = "nevsehir"
+var ülke:String = "Türkiye"
+var telefon:String = "05375003424"
+var postaKodu:Double = 50000
+var email:String = "ahmetkardesseven@gmail.com"
+var meslek:String = "Mühendis"
+var stokMiktari:Int = 20
+var müsteriAdi:String = "ahmet"
+var bakiye:Int = 200
+var dogumGünü:String = "14 Subat"
+var maas:Int = 0
+var medeniDurum = "Bekar"
+var ürünYorumu = "iyi"
+var ödemeTarihi = "14.10.2023"
+var ödeme = "pesin"
+var siparisAdedi = 10
+var arabaModeli = "BMW"
+var kitapAdi = "Suc ve Ceza"
+var yayınlanmaTarihi = 2010
+var indirimMiktari = "%50"
+var odaSayisi = 4
+var enlem = 42
+var boylam = 54
+var ürünAdi = "Monitör"
+var yemekFiyati = 200
+var marka = "apple"
+var muzikAdi = "Eller Günahkar"
+var videoSüresi = "1 saat"
+var ürünPuani = 10
+var ResimAdi = "Vazo"
+var dosyaFormati = "png"
+var renk = "siyah"
+var renkKodu = "ff12Ab"
+var telefonModeli = "iphone 13"
+var ekranBoyutu = "27 inc"
+var agirlik = 20
+var ulusalGün = "23 Nisan"
+var tatilGunu = "Pazar"
+var rezervasyonTarihi = "14.10.2022"
+var sokakAdi = "15 Temmuz"
+var otobusHatti = "500T"
+var kalanDakika = 13
+var takipKodu = 232212
+var kuponSuresi = "10 gün"
+var kuponKodu = 10100110
+var faturaAdresi = "Sakarya/Serdivan"
+
+
